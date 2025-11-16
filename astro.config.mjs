a81389438323d91a9e702216d39e068867fb949f
Igor Makarov <igormaka@gmail.com>
2025-11-16 09:30:14 +0200

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://igor-makarov.github.io',
   base: '/DnD-Playing',
+  integrations: [react()],
 })
