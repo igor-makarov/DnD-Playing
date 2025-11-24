6ec42eda0b58122e192b098d645f14c7eb04921e
Igor Makarov <igormaka@gmail.com>
2025-11-24 12:33:45 +0200

    cleanup

--- src/js/hooks/useRollMode.ts
+++ src/js/hooks/useRollMode.ts
@@ -1,6 +1,6 @@
 import { useSyncExternalStore } from "react";
 
-import { type RollMode, rollModeStore } from "./rollModeStore";
+import { type RollMode, rollModeStore } from "@/js/stores/rollModeStore";
 
 export function useRollMode() {
   return useSyncExternalStore<RollMode>(rollModeStore.subscribe, rollModeStore.get, () => "app");
