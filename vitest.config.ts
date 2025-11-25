a4ab59ac8d0644131873b687baba3c18a46c26df
Igor Makarov <igormaka@gmail.com>
2025-11-25 21:24:47 +0200

    add playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    exclude: ["**/node_modules/**", "**/dist/**", "**/e2e/**"],
     coverage: {
       provider: "v8",
       reporter: ["text", "html", "lcov"],
