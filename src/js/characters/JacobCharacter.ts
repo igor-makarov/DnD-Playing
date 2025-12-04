3a7f61abd8f3aba6d96b5530021d52db17b2d742
Igor Makarov <igormaka@gmail.com>
2025-12-04 12:24:02 +0200

    add jacob

---
+++ src/js/characters/JacobCharacter.ts
@@ -0,0 +1,60 @@
+import { Character } from "@/js/character/Character";
+import { D20Test } from "@/js/common/D20Test";
+import { DiceString } from "@/js/common/DiceString";
+
+export default class JacobCharacter extends Character {
+  constructor() {
+    super({
+      abilityScores: {
+        Str: 13,
+        Dex: 16,
+        Con: 15 + 1 /* Background +1 */,
+        Int: 11,
+        Wis: 9,
+        Cha: 18 + 2 /* Background +2 */,
+      },
+      classLevels: [{ className: "Rogue", level: 1 }],
+
+      skillProficiencies: [
+        { skill: "Deception" }, // Rogue
+        { skill: "Intimidation" }, // Rogue
+        { skill: "Persuasion", multiplier: 2 }, // Rogue with Expertise
+        { skill: "Stealth", multiplier: 2 }, // Rogue with Expertise
+        { skill: "Performance" }, // Background
+        { skill: "Athletics" }, // Background
+        { skill: "Acrobatics" }, // Human
+      ],
+      saveProficiencies: [
+        { save: "Dex" }, // Rogue
+        { save: "Int" }, // Rogue
+      ],
+      weapons: [
+        { weapon: "Handaxe (Vex)", ability: "Dex", damage: new DiceString("d6") },
+        { weapon: "Dagger (Nick)", ability: "Dex", damage: new DiceString("d4") },
+        { weapon: "Shortbow", ability: "Dex", damage: new DiceString("d6") },
+      ],
+      attackAddons: [{ name: "Sneak Attack", damage: { optional: true, damage: new DiceString("d6") } }],
+      hitPointRolls: [{ level: 1, die: new DiceString("d8"), roll: 8 }],
+    });
+  }
+
+  // Spell Attack Modifier: Charisma modifier + Proficiency bonus (Magic Initiate)
+  getSpellAttack(): D20Test {
+    return new D20Test("Attack Roll", "Cha", this.getAbilityModifier("Cha"), this.createProficiency(true));
+  }
+
+  // Spell Save DC: 8 + Proficiency bonus + Charisma modifier
+  getSpellSaveDC(): number {
+    return 8 + this.getSpellAttack().getBonus();
+  }
+
+  // AC with Mage Armor: 13 + DEX modifier
+  getArmorClass(): number {
+    return 13 + this.getAbilityModifier("Dex");
+  }
+
+  // Initiative with Alert feat: DEX modifier + Proficiency bonus
+  getInitiative(): D20Test {
+    return new D20Test("Ability Check", "Dex", this.getAbilityModifier("Dex"), this.createProficiency(true));
+  }
+}
