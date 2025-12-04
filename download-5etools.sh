43e40ec1f5f51277fdcf5a35e950ebcb4b783f10
Igor Makarov <igormaka@gmail.com>
2025-12-04 19:07:16 +0200

    get rid of submodules

---
+++ download-5etools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Download 5etools data
+VERSION="v2.19.1"
+URL="https://github.com/5etools-mirror-3/5etools-src/releases/download/${VERSION}/5etools-${VERSION}.zip"
+OUTPUT_DIR="5etools"
+
+echo "Downloading 5etools ${VERSION}..."
+curl -L -o 5etools.zip "$URL"
+
+echo "Extracting..."
+rm -rf "$OUTPUT_DIR"
+unzip -q 5etools.zip -d "$OUTPUT_DIR"
+
+echo "Cleaning up..."
+rm 5etools.zip
+
+echo "Done! 5etools data is ready in ${OUTPUT_DIR}/"
