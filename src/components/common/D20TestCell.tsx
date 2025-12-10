677faf83497f6849a3ab20ae2ddd2912d1ebb1c0
Igor Makarov <igormaka@gmail.com>
2025-12-10 19:59:17 +0200

    renames

--- src/components/common/D20TestCell.tsx
+++ src/components/common/D20TestCell.tsx
@@ -1,11 +1,5 @@
-import { D20Test } from "@/js/common/D20Test";
 import { withAutoRehydration } from "@/js/utils/withAutoRehydration";
 
-import D20TestCellClient from "./D20TestCellClient";
-
-interface Props {
-  roll: D20Test;
-  advantage?: boolean;
-}
+import D20TestCellClient from "./D20TestCell.client";
 
 export default withAutoRehydration(D20TestCellClient);
