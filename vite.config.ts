93d203d6f78415bad00f101177f6ee49aae973f4
Igor Makarov <igormaka@gmail.com>
2025-12-15 09:39:15 +0200

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
-import remarkFrontmatter from "remark-frontmatter";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,13 +7,5 @@ export default defineConfig({
   build: {
     assetsDir: (process.env.BASE_URL || "/").substring(1) + "assets/",
   },
-  plugins: [
-    {
-      enforce: "pre",
-      ...mdx({ remarkPlugins: [remarkFrontmatter] }),
-    },
-
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [tsconfigPaths(), mdx(), reactRouter()],
 });
