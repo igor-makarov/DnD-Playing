00af3da02d05acddb24d73818b9e7011cca1c1ef
Igor Makarov <igormaka@gmail.com>
2026-01-01 17:05:41 +0200

    rewrite

--- src/components/CharacterNameTable.tsx
+++ src/components/CharacterNameTable.tsx
@@ -16,16 +16,11 @@ export default function CharacterNameTable({ name, infoHref }: Props) {
         </tr>
         <tr>
           <td colSpan={2} style={{ textAlign: "center" }}>
-            {infoHref ? (
-              <>
-                {name} (
-                <Link to={href(infoHref)} target="_blank">
-                  build info
-                </Link>
-                )
-              </>
-            ) : (
-              name
+            {name}{" "}
+            {infoHref && (
+              <Link to={href(infoHref)} target="_blank">
+                build info
+              </Link>
             )}
           </td>
         </tr>
